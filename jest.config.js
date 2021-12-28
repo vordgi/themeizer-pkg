15f5e20cb466405ddc69fd4b6f9520b277b48946
Vordgi <sasha2822222@gmail.com>
2021-12-28 16:46:57 +0200

    T-19 configure jest

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverageFrom: ["src/**/*.ts", "!src/tests/*.ts", "!src/types/*.ts"],
+  coverageReporters: ["text"],
+  testRegex: "src/tests/.*\\.test\\.ts$",
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
+};
